6b63afdf4f19edcc67cc28119ebba8aef0c29eb9
ermaldo <22beer1bif@hft-stuttgart.de>
2015-11-25 02:58:37 +0100

    necessary files for style and menu

---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ermaldo on 25.11.15.
+ */
+
+
+// for new menu items just add thuse below by adding or acting as shown
+document.getElementById("nav01").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='globaldata.html'>Global</a></li>" +
+    "<li><a href='analyze-me.html'>Analyze Me</a></li>" +
+    "</ul>";
+
