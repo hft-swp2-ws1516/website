6b550079e4b0e74b017ac99ad43828076cca32ca
Lukas1Komarek <lukaskomarek@web.de>
2015-11-26 20:58:33 +0100

    add bootstrap fixed to top navbar

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,9 +5,11 @@
 
 // for new menu items just add thuse below by adding or acting as shown
 document.getElementById("nav01").innerHTML =
-    "<ul id='menu'>" +
-    "<li><a href='index.html'>Home</a></li>" +
-    "<li><a href='globaldata.html'>Global</a></li>" +
-    "<li><a href='analyze-me.html'>Analyze Me</a></li>" +
-    "</ul>";
+"<div class='container'>"+
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='globaldata.html'>Global</a></li>" +
+"<li><a href='analyze-me.html'>Analyze Me</a></li>" +
+"</ul>"+
+"</div>";
 
