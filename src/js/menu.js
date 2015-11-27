b36d2d1ee374d522725a6b56dfe91359be577874
Lukas1Komarek <lukaskomarek@web.de>
2015-11-27 16:01:31 +0100

    added bootstrap columns and smaller layout options

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,10 +6,14 @@
 // for new menu items just add thuse below by adding or acting as shown
 document.getElementById("nav01").innerHTML =
 "<div class='container'>"+
-"<ul id='menu'>" +
+"<div class='row'>"+
+"<div class='col-md-4'><ul id='menu'>" +
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='globaldata.html'>Global</a></li>" +
 "<li><a href='analyze-me.html'>Analyze Me</a></li>" +
-"</ul>"+
+"</ul></div>"+
+"<div class='col-md-4 col-md-offset-4'>Last Scan: DD.MM.YYYY</div>"+
+
+"</div>"+
 "</div>";
 
