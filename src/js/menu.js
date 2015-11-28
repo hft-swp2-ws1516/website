d453deb864ca1e255a21e62f665bf921c7db2fd1
ermaldo <22beer1bif@hft-stuttgart.de>
2015-11-28 11:54:01 +0100

    Merge branch 'master' of /home/ermaldo/Dokumente/website with conflicts.

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,20 +5,9 @@
 
 // for new menu items just add thuse below by adding or acting as shown
 document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='globaldata.html'>Global</a></li>" +
-"<li><a href='analyze-me.html'>Analyze Me</a></li>" +
-"<li><a href='allgraphs.html'>All Graphs</a></li>" +
-"</ul>";
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='globaldata.html'>Global</a></li>" +
+    "<li><a href='crawlUrl.html'>Analyze Me</a></li>" +
+    "</ul>";
 
-
-// for footer menu
-document.getElementById("footer").innerHTML =
-"&copy; TLS Crawler"+
-"<div>"+ 
-"Ein Projekt der HfT Stuttgart."+
-"&#124;<a href='url'>Kontakt</a>"+
-"&#124;<a href='url'>Impressum</a>"+
-"&#124;<a href='url'> weitere Links </a>"+
-"</div>";
