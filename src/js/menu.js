e71c7f0b4dbb6fea238ab7db16df9dae00984b50
ermaldo <22beer1bif@hft-stuttgart.de>
2015-12-07 22:02:54 +0100

    implemented the subscribe function via subscribtion.php

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,17 +5,17 @@
 
 // for new menu items just add thuse below by adding or acting as shown
 document.getElementById("nav01").innerHTML =
-"<div class='container'>"+
-"<div class='row'>"+
-"<div class='col-md-4'><ul class='nav navbar-nav'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='globaldata.html'>Global</a></li>" +
-"<li><a href='analyze-me.html'>Analyze Me</a></li>" +
-"</ul></div>"+
-"<div class='col-md-4 col-md-offset-4'>Last Scan: DD.MM.YYYY</div>"+
+    "<div class='container'>"+
+    "<div class='row'>"+
+    "<div class='col-md-4'><ul class='nav navbar-nav'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='globaldata.html'>Global</a></li>" +
+    "<li><a href='analyze-me.html'>Analyze Me</a></li>" +
+    "</ul></div>"+
+    "<div class='col-md-4 col-md-offset-4'>Last Scan: DD.MM.YYYY</div>"+
 
-"</div>"+
-"</div>";
+    "</div>"+
+    "</div>";
 
 
 
@@ -23,42 +23,75 @@ document.getElementById("nav01").innerHTML =
 var n = new Date();
 var d = n.getFullYear(); // dynamic Date
 document.getElementById("footer").innerHTML =
-"<div class='container'>"+
-" &copy; "+d+" TLS Crawler ist "+
-" ein Projekt der HfT Stuttgart. "+
-" &#124;<a href='url'> Kontakt</a> "+
-" &#124;<a href='url'> Impressum</a> "+
-" &#124;<a href='#subscription' data-toggle='modal'> Subscribe </a> "+
-"</div>";
-
-// open Subscribe Modal
-document.getElementById("subscribe").innerHTML =
     "<div class='container'>"+
-    "<div class='modal fade' id='subscription' role='dialog'>"+
+    " &copy; "+d+" TLS Crawler ist "+
+    " ein Projekt der HfT Stuttgart. "+
+    " &#124;<a href='url'> Kontakt</a> "+
+    " &#124;<a href='url'> Impressum</a> "+
+    " &#124;<a href='#subscribtion' id='sub' data-toggle='modal'> Subscribe </a> "+
+    "</div>";
+
+// open Subscribe Modal and this modal will open the subscriber count modal on subscribtion.php file after the use press on save button
+document.getElementById("subscribe").innerHTML =
+    "<form method='post' action='subscribtion.php'>"+
+    "<div class='container'>"+
+    "<div class='modal fade' id='subscribtion' role='dialog'>"+
     "   <div class='modal-dialog'>"+
     "       <div class='modal-content'>"+
     "           <div class='modal-header'>"+
     "               <button type='button' class='close' data-dismiss='modal'>&times;</button>"+
-    "               <h4 class='modal-title'><b>Subscribe for get e-mailed</b></h4>"+
+    "               <h4 class='modal-title'><b>Subscribe for e-mail:</b></h4>"+
     "           </div>"+
     "           <div class='modal-body'>"+
     "               <div class='form-group'>"+
-    "                   <label for='name'>Your Name:</label>"+
-    "                   <input type='email' class='form-control' id='name' placeholder='Your Name'>"+
+    "                   <label for='name'>Name & Surname:</label>"+
+    "                   <input type='text' class='form-control' name='name' id='name' placeholder='Your Name'>"+
     "               </div>"+
     "               <div class='input-group input-group-lg'>"+
     "                   <span class='input-group-addon' id='sizing-addon1'>@</span>"+
-    "                   <input type='text' class='form-control' placeholder='Email' aria-describedby='sizing-addon1'>"+
+    "                   <input type='email' class='form-control' name='email' placeholder='Email' aria-describedby='sizing-addon1'>"+
     "               </div><br><hr>"+
     "               <div>"+
-    "                   <input id='agree' name='radio' type='radio' class='radio'>"+
-    "                   <label for='agree'>I hereby agree to receive e-mails!</label>"+
+    "                   <table>" +
+    "                       <tr>" +
+    "                           <td>" +
+    "                               <h5>Subscribe:</h5>" +
+    "                           </td>" +
+    "                           <td>" +
+    "                               <div class='onoffswitch'>" +
+    "                                   <input type='checkbox' name='onoffswitch' class='onoffswitch-checkbox' id='myonoffswitch'>" +
+    "                                   <label class='onoffswitch-label' for='myonoffswitch'>" +
+    "                                       <span class='onoffswitch-inner'></span>" +
+    "                                       <span class='onoffswitch-switch'></span>" +
+    "                                   </label>" +
+    "                               </div>" +
+    "                           </td>" +
+    "                       </tr>" +
+    "                   </table>" +
     "               </div>"+
     "           </div>"+
-    "       <div class='modal-footer'>"+
-    "           <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>"+
-    "           <button type='button' class='btn btn-success'>Save</button>"+
+    "       <div class='modal-footer'>" +
+    "       <table>" +
+    "       <tbody>" +
+    "       <tr>" +
+    "           <td>" +
+    "               <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>"+
+    "               <button type='submit' class='btn btn-success' data-toggle='modal' data-target='#subModal' id='save' disabled>Save</button>" +
+    "           </td>" +
+    "       </tr>" +
+    "       </tbody>" +
+    "       </table>"+
     "       </div>"+
     "   </div>"+
     "</div>"+
-    "</div>";<!--End of Modal-Window-->
+    "</div>"+
+    "</form>";<!--End of Modal-Window-->
+
+// disabling/enable the save button
+$("#myonoffswitch").on("click", function()
+{
+    var toggle = $("#save").prop("disabled");
+
+    $("#save").prop("disabled", !toggle );
+});
+// end Subscribe part
