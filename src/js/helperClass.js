603ebc60228801846e18a3db5d20d85e4e825700
Lukas1Komarek <lukaskomarek@web.de>
2015-12-14 20:36:33 +0100

    Implemented first version of Ciphers Summary Graph + small corrections

--- src/js/helperClass.js
+++ src/js/helperClass.js
@@ -1,3 +1,62 @@
+/* Get Timespan from Datepicker or Timeslider, depending which of them is visible
+   return : array with first element 1st datetime in ms and second 2nd datetime in ms
+   */
+
+   function getTimespan(){
+    timespan = [];
+    // Check which of the datepickers are visible
+    if ($('#timeslider').is(":visible") ) {
+      timespan = dateSlider.noUiSlider.get();
+    }
+    else{
+      var time1 =  new Date($('#filterDateStart').datepicker('getDate').getTime());
+      var time2 = new Date($('#filterDateEnd').datepicker('getDate').getTime());
+
+      timespan[0] = (new Date(time1.getFullYear(), time1.getMonth())).getTime();
+      timespan[1] =  (new Date(time2.getFullYear(), time2.getMonth())).getTime();
+      //alert("Datepicker: Time1:"+ timespan[0] + "Time2:" + timespan[1]);
+    }
+
+    return timespan;
+  }
+
+
+/* Merge double Entry of Ciphersuites from different TLS Versions in the ciphers/summary response JSON 
+   returns summarized array
+   */
+
+   function mergeDoubleEntrys(json){
+     var summarized = [];
+     for (var i = 0; i < json.length; i++) {
+      var doubleEntry = false;
+      // get a object from the summary
+      var obj = json[i];
+      // iterate over summarized array and check for double entry
+      for (var j = 0; j < summarized.length; j++) {
+      // if entry found, add increase the count number of the first entry
+      if (obj['cipher'] == summarized[j].cipher ) {
+        doubleEntry = true;
+        summarized[j].count += obj['count'];
+      };
+    };
+       // if no double entry found, push the obj normally to the summarized arra
+       if (!doubleEntry) {
+        summarized.push(obj);
+      };
+      doubleEntry = false;
+    };
+
+    return summarized;
+  }
+
+
+ // Compare function for sorting Array of JSONs 
+ function sortResults(arr, prop, asc) {
+  arr = arr.sort(function(a, b) {
+    if (asc) return (a[prop] > b[prop]);
+    else return (b[prop] > a[prop]);
+  });
+}
 
 function formatDate2(date) {
 	var string = date.getFullYear() + "_" + (date.getMonth() + 1);
