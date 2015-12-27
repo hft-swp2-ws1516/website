3cecb9ce46f2de1c2ebcedc1bb73fc8763c14d87
ermaldo <22beer1bif@hft-stuttgart.de>
2015-12-27 14:10:43 +0100

    Modified mongo.php. "new Mongo() is deprecated" instead  "new MongoClient()".

--- src/php/mongo.php
+++ src/php/mongo.php
@@ -4,9 +4,10 @@
  * User: ermaldo
  * Date: 05.12.15
  * Time: 20:17
+ * Change Db-Nmae, Collection name as you like
  */
 
 
-$n = new Mongo(); // Creating an Instance of MongoDB
-$db = $n->learningmongo; // Creating the database learningmongo
-$people = $db->people; // Creating the Collection People
+        $n = new MongoClient(); // Creating an Instance of MongoDB
+        $db = $n->learningmongo; // Creating the database learningmongo
+        $people = $db->people; // Creating the Collection People
