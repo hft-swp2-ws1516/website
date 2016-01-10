0a9a42b9cd9e3e8a2fd3a03c12b403be702253f4
ermaldo <22beer1bif@hft-stuttgart.de>
2016-01-10 23:38:00 +0100

    added custom Error handling servererrormessage.php file for different Server Codes

---
+++ src/php/servererrormessage.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ermaldo
+ * Date: 10.01.16
+ * Time: 21:13
+ */
+
+$status = $_SERVER['REDIRECT_STATUS'];
+$codes = array(
+    403 => array('403 Forbidden', 'You are not authorized to access this file.'),
+    404 => array('404 File Not Found', 'The document/file requested was not found on this server.'),
+    405 => array('405 Method Not Allowed', 'The method specified in the Request-Line is not allowed for the specified resource.'),
+    408 => array('408 Request Timeout', 'The browser failed to send a request in the time allowed by the server.'),
+    500 => array('500 Internal Server Error', 'Your request could not be proceeded because of an internal server error.'),
+    502 => array('502 Bad Gateway', 'The server received aan invalid response.'),
+    504 => array('504 Bad Gateway Timeout', 'The upstream server failed to send a request in the time allowed by the server.'),
+);
+
+$title = $codes[$status][0];
+$message = $codes[$status][1];
+
+if($title == false || strlen($status) != 3)
+{
+    $title = 'Unrecognized Status Code';
+    $message = 'The website returned an unrecognised status code';
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+    <style>
+        html, body{
+            font-size: 100%;
+        }
+
+        body{
+            font-family: Arial, sans-serif;
+            background: #f7f7f7;
+            margin: 0;
+            padding: 0;
+        }
+
+        #content{
+            box-sizing: border-box;
+            max-width: 100%;
+            width: 800px;
+            margin: 50px auto;
+            padding: 50px;
+            background: #fff;
+        }
+
+        h1{
+            margin-top: 0;
+        }
+
+        p{
+            font-size: 1rem;
+            line-height: 1.5;
+        }
+    </style>
+</head>
+<body>
+    <div id="content">
+        <h1><?php echo $title; ?></h1>
+        <p><?php echo $message; ?></p>
+    </div>
+</body>
+</html>
