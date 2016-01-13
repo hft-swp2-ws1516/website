904c83ffa838a639b258e9e72f645b6fc74de49f
ermaldo <22beer1bif@hft-stuttgart.de>
2016-01-13 16:20:50 +0100

    Checking if the entered email is unique. If not message modal will show up

--- src/php/subscribe.php
+++ src/php/subscribe.php
@@ -12,8 +12,8 @@
  *
  */
 
-include('mongo.php'); /** no need for adjusting  */
-include('../index.html');/** should be the right path */
+include('mongo.php'); /** path should be adjusted later on the server */
+include('../index.html');/** path should be adjusted later on the server */
 /** Checking if the name-field is not blank */
 if(isset($_POST['name']) && !empty($_POST['name']) AND isset($_POST['email']) && !empty($_POST['email']))
 {
@@ -30,47 +30,93 @@ if(isset($_POST['name']) && !empty($_POST['name']) AND isset($_POST['email']) &&
         // If the link has been clicked once the activation will change to 1
         $active  = "0";
 
-        $lastscan = new DOMDocument();
-        $lastscan->getElementById('lastScan');
+        $emailIsUnique = false;
+    // check connection and connect
+    try{
+        $connect = $n->selectDB("learningmongo");
+        $people = $connect->selectCollection("people");
+    }catch(MongoConnectionException $e){
+        die("Problem during mongodb initialization. Please start mongodb server." . $e->getMessage());
+    }
+
     if(!preg_match("/^([a-zA-z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$/", $email)||empty($name))
     {
         // Return an Error if Mail doe not match. Server side looking
         $msg = "Please fill in the name field and enter a valid name";
     }else{
         // Return success - Valid email and name field not empty
-        $msg = "<b>".$name."</b>"." one last step is needed in order to complete your Subscribing. <br/> Please activate your Subscription by clicking the link we have sent to "."<a href='#'>".$email."</a>"." <br/> Thanks to Subscriber";
+        $msg = "<b>".$name."</b> one last step is needed in order to complete your Subscribing. <br/>";
+        $msg .="Please activate your Subscription by clicking the link we have sent to <a href='#'>".$email."</a><br/>";
         try{
             /** if not blank we will insert the given values in the collection */
-            $people->insert( array(
-                "name"              => $name,
-                "email"             => $email,
-                "hash_subscribe"    => $hash_subscribe,
-                "hash_unsubscribe"  => $hash_unsubscribe,
-                "active"            => $active
-            ));
+            if($connect)
+            {
+                $testEmailUnique = $people->find(
+                    array('email' => $email)
+                );
+                // look for if the email is unique.
+                $are = $testEmailUnique->count();
 
-            // Send the email
-            $to          = $email;
-            $subject     = "Subscribing for Crawler-Results";
-            $messageBody = "<!doctype html><html class='no-js' lang=''>";
-            $messageBody .="<head><meta charset='utf-8'><meta http-equiv='x-ua-compatible' content='ie=edge'>";
-            $messageBody .="<title>Experimental Study on TLS-Handshakes</title><meta name='description' content=''>";
-            $messageBody .="<meta name='viewport' content='width=device-width, initial-scale=1'>";
-            $messageBody .= "<link href='../css/bootstrap.min.css' rel='stylesheet'><meta charset='utf-8'>";
-            $messageBody .="<meta name='viewport' content='width=device-width, initial-scale=1'>";
-            $messageBody .="<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>";
-            $messageBody .="<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'>";
-            $messageBody .="</script><script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'>";
-            $messageBody .="</script></head><body><div class='container'><div class='content-wrapper'>";
-            $messageBody .="<h1>Crawling-Results are right on the way to you!</h1><hr>";
-            $messageBody .="<p>Tanks for subscribing! We will sent as soon as possible the";
-            $messageBody .="Crawling-Results to " .$name. " and " .$email. " </p><hr>";
-            $messageBody .="<p>Please click the link to activate your account:";
-            $messageBody .="https://hotcat.de/php/verify.php?email=$email&hash_subscribe=$hash_subscribe"; // Should work
-            $messageBody .="<br/>Thank you for subscribing!</p></div></div></body></html>";
-            $headers     = "From: noreply@hotcat.de" . "\r\n"; // set from headers
-            $headers     .= "Content-type: text/html; charset=iso-8859-1\n";
-            mail($to, $subject, $messageBody, $headers); // Send mail
+                if($are == 0)
+                {
+                    $emailIsUnique = true;
+                    $people->insert( array(
+                        "name"              => $name,
+                        "email"             => $email,
+                        "hash_subscribe"    => $hash_subscribe,
+                        "hash_unsubscribe"  => $hash_unsubscribe,
+                        "active"            => $active
+                    ));
+
+
+                    // Send the email
+                    $to          = $email;
+                    $subject     = "Subscribing for Crawler-Results";
+                    $messageBody = "<!doctype html><html class='no-js' lang=''>";
+                    $messageBody .="<head><meta charset='utf-8'><meta http-equiv='x-ua-compatible' content='ie=edge'>";
+                    $messageBody .="<title>Experimental Study on TLS-Handshakes</title><meta name='description' content=''>";
+                    $messageBody .="<meta name='viewport' content='width=device-width, initial-scale=1'>";
+                    $messageBody .= "<link href='../css/bootstrap.min.css' rel='stylesheet'><meta charset='utf-8'>";
+                    $messageBody .="<meta name='viewport' content='width=device-width, initial-scale=1'>";
+                    $messageBody .="<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>";
+                    $messageBody .="<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'>";
+                    $messageBody .="</script><script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'>";
+                    $messageBody .="</script></head><body><div class='container'><div class='content-wrapper'>";
+                    $messageBody .="<h1>Crawling-Results are right on the way to you!</h1><hr>";
+                    $messageBody .="<p>Tanks for subscribing! We will sent as soon as possible the";
+                    $messageBody .="Crawling-Results to " .$name. " and " .$email. " </p><hr>";
+                    $messageBody .="<p>Please click the link to activate your account: ";
+                    $messageBody .="http://localhost/src/php/verify.php?email=$email&hash_subscribe=$hash_subscribe";
+                    $messageBody .="<br/>"; // You should adjust the Path
+                    $messageBody .="http://localhost/src/php/unsubscribe.php?email=$email&hash_unsubscribe=$hash_unsubscribe";
+                    $messageBody .="<br/>Thank you for subscribing!</p></div></div></body></html>";
+                    $headers     = "From: geermebe@gmail.com" . "\r\n"; // set from headers
+                    $headers     .= "Content-type: text/html; charset=iso-8859-1\n";
+                    mail($to, $subject, $messageBody, $headers); // Send mail
+
+                    //$mime = new Mail_mime();
+                    //$mime->setTXTBody($messageBody);
+                    //$from = "TLS Crawler Team";
+                    //$body = $mime->get();
+                    //$header = $mime->headers(
+                    //    array(
+                    //        'From' => 'tls.hotcat@gmail.com',
+                    //        'To' => $to,
+                    //        'Subject' => $subject
+                    //    )
+                    //);
+
+                    //$smtp = Mail::factory('smtp', array(
+                    //    'host' => 'ssl://smtp.googlemail.com',
+                    //    'port' => '465',
+                    //    'auth' => true,
+                    //    'username' => 'tls.hotcat',
+                    //    'password' => 'hotdog2015-2016',
+                    //));
+
+                    //$smtp->send($to, $header, $messageBody); // send mail
+                }
+            }
         }catch(Exception $e){
             echo "Message: ".$e->getMessage();
         }
@@ -115,7 +161,9 @@ $cursor = $people->find(); //handling with result
                                 <?php
                                     if(empty($name) || empty($email))
                                     {
-                                        $msg = "You should fill the name and email fields! Please try again.";
+                                        $msg = "You should fill the name and email fields! Please try again.<br>";
+                                    }elseif($emailIsUnique == false){
+                                        $msg = "<a href='#'>".$email."</a> is taken by an other Subscriber. Please try an other e-mail!<br>";
                                     }else{
                                         $msg = $msg;
                                     }
@@ -124,7 +172,7 @@ $cursor = $people->find(); //handling with result
                                 <strong>
                                     <span class="badge">
                                         <?php
-                                            echo $cursor->count($_GET['_id']);
+                                            echo "In total, they are " . $cursor->count($_GET['_id']) . " Subscriber";
                                         ?>
                                     </span>
                                 </strong>
@@ -160,6 +208,6 @@ $cursor = $people->find(); //handling with result
         this.location.href="../index.html";
     }
 </script>
-<script src="../js/main/menu.js"></script>
+<script src="../js/menu.js"></script>
 </body>
 </html>
